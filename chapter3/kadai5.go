89249fef7f68d21364413ac733131a0f89d64997
pisces6 <taka.pisces6@gmail.com>
2020-02-06 21:07:56 +0900

    chapter3

--- chapter3/kadai5.go
+++ chapter3/kadai5.go
@@ -1,5 +1,7 @@
 package chapter3
 
+import "fmt"
+
 // 課題5
 // とあるマスターデータのテーブルの構造をモデル化したstructを作りました。
 // idやnameを変更できないようにカプセル化し、Getterのみ実装しました。
@@ -19,4 +21,9 @@ func (m Master) ID() int {
 
 func (m Master) Name() string {
 	return m.name
+}
+
+func (m Master) MarshalJSON() ([]byte, error) {
+	s := fmt.Sprintf("{\"id\": %d, \"name\": \"%s\"}", m.ID(), m.Name())
+	return []byte(s), nil
 }
